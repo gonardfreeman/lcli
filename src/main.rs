06de406d0b1bec26193943969cf3383a13edb048
Dmytro Bondarenko <007arenko@gmail.com>
2026-03-21 10:21:51 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use clap::{Parser /*, Subcommand */};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about= None)]
+struct Args {
+    #[arg(short, long, default_value = "shuttle")]
+    name: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("Hello, {}!", args.name);
+}
